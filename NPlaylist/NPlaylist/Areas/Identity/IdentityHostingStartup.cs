72b9a7edc6619928c1dbe9671412676e5418d276
Gheorghe Ceapchii <ceapchiigeorge@gmail.com>
2019-04-11 13:27:38 +0300

    Initial structure

---
+++ NPlaylist/NPlaylist/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NPlaylist.Data;
+
+[assembly: HostingStartup(typeof(NPlaylist.Areas.Identity.IdentityHostingStartup))]
+namespace NPlaylist.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

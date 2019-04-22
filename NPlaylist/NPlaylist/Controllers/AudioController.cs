1bfea28de7147cb456fb062bfa9bdfda0cd5de1b
Gheorghe Ceapchii <ceapchiigeorge@gmail.com>
2019-04-22 15:25:28 +0300

    Added Song List View
    Added controller and a simple page with dummy data

---
+++ NPlaylist/NPlaylist/Controllers/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NPlaylist.Models;
+using System.Collections.Generic;
+using NPlaylist.Infrastructure.Wrappers.DateTimeWrapper;
+
+namespace NPlaylist.Controllers
+{
+    public class AudioController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<AudioEntryViewModel> entries = new List<AudioEntryViewModel>();
+            entries.Add(new AudioEntryViewModel
+            {
+                Id = Guid.NewGuid(),
+                Extension = "mp3",
+                FullName = "Foo - Bar",
+                Publisher = "Baz",
+                Created = DateTime.Now
+            });
+            return View(entries);
+        }
+    }
+}

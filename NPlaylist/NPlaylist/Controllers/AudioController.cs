8698fd0e058850dffb8951d245579f6abef0f291
Gheorghe Ceapchii <ceapchiigeorge@gmail.com>
2019-04-22 17:28:46 +0300

    Unit test added

--- NPlaylist/NPlaylist/Controllers/AudioController.cs
+++ NPlaylist/NPlaylist/Controllers/AudioController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using NPlaylist.Models;
 using System.Collections.Generic;
-using NPlaylist.Infrastructure.Wrappers.DateTimeWrapper;
 
 namespace NPlaylist.Controllers
 {
@@ -10,15 +9,17 @@ namespace NPlaylist.Controllers
     {
         public IActionResult Index()
         {
-            IList<AudioEntryViewModel> entries = new List<AudioEntryViewModel>();
-            entries.Add(new AudioEntryViewModel
+            IList<AudioEntryViewModel> entries = new List<AudioEntryViewModel>
             {
-                Id = Guid.NewGuid(),
-                Extension = "mp3",
-                FullName = "Foo - Bar",
-                Publisher = "Baz",
-                Created = DateTime.Now
-            });
+                new AudioEntryViewModel
+                {
+                    Id = Guid.NewGuid(),
+                    Extension = "mp3",
+                    FullName = "Foo - Bar",
+                    Publisher = "Baz",
+                    Created = DateTime.Now
+                }
+            };
             return View(entries);
         }
     }

1f39f3c635a5fcda65e249ad106831a76df7375a
Gheorghe Ceapchii <ceapchiigeorge@gmail.com>
2019-04-26 14:50:54 +0300

    View and Controller Parts for Delete

--- NPlaylist/NPlaylist/Controllers/AudioController.cs
+++ NPlaylist/NPlaylist/Controllers/AudioController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NPlaylist.Models;
+using NPlaylist.Models.Audio;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace NPlaylist.Controllers
 {
@@ -21,6 +24,36 @@ namespace NPlaylist.Controllers
                 }
             };
             return View(entries);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Delete(Guid id)
+        {
+            var audioViewModel = new AudioViewModel
+            {
+                AudioId = id,
+                UtcCreatedTime = DateTime.Now,
+                Path = "Foo/Bar",
+                Meta = new AudioMetaViewModel
+                {
+                    Title = "John",
+                    Author = "Doe"
+                }
+            };
+            return View(audioViewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Delete(Guid id, CancellationToken ct)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Delete");
         }
     }
 }
---
+++ NPlaylist/NPlaylist/Models/Audio/AudioViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NPlaylist.Models.Audio
+{
+    public class AudioViewModel
+    {
+        public Guid AudioId { get; set; }
+        public Guid OwnerId { get; set; }
+        public DateTime UtcCreatedTime { get; set; }
+        public string Path { get; set; }
+        public AudioMetaViewModel Meta { get; set; }
+    }
+}

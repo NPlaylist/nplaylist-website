c4c446d01370566b2ffcc7ab801ce77b51bf947d
Max <maksim.podlesnov@mail.ru>
2019-04-30 14:23:33 +0300

    Upload file view
    Extension validator
    
    Fix

---
+++ NPlaylist/NPlaylist/Models/AudioUploadViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace NPlaylist.Models
+{
+    public class AudioUploadViewModel
+    {
+        [Required(ErrorMessage = "Select audio file.")]
+        [FileExtensions(Extensions = "mp3,wav", ErrorMessage = ("File format is wrong."))]
+        public IFormFile File { get; set; }
+
+        public string PublisherId { get; set; }
+    }
+}

ff1a45b677e76809a415f51f6e22a2eb53a86c85
Max <maksim.podlesnov@mail.ru>
2019-05-13 14:41:23 +0300

    View part for create playlist

---
+++ NPlaylist/NPlaylist/Controllers/PlaylistController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NPlaylist.ViewModels.Playlist;
+using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NPlaylist.Controllers
+{
+    [Authorize]
+    public class PlaylistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PlaylistCreateViewModel playlistCreateViewModel, CancellationToken ct)
+        {
+            if (ModelState.IsValid)
+            {
+                playlistCreateViewModel.OwnerId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ NPlaylist/NPlaylist/ViewModels/Playlist/PlaylistCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NPlaylist.ViewModels.Playlist
+{
+    public class PlaylistCreateViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
